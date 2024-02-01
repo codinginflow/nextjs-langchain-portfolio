16909824ec28f591fc4345c5ef9065b40cfb1607
Coding in Flow <info@codinginflow.com>
2024-02-01 11:50:37 +0100

    End of part 10

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,6 +1,9 @@
-import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { getVectorStore } from "@/lib/astradb";
+import { ChatPromptTemplate, PromptTemplate } from "@langchain/core/prompts";
 import { ChatOpenAI } from "@langchain/openai";
 import { LangChainStream, StreamingTextResponse } from "ai";
+import { createStuffDocumentsChain } from "langchain/chains/combine_documents";
+import { createRetrievalChain } from "langchain/chains/retrieval";
 
 export async function POST(req: Request) {
   try {
@@ -15,19 +18,38 @@ export async function POST(req: Request) {
       modelName: "gpt-3.5-turbo",
       streaming: true,
       callbacks: [handlers],
+      verbose: true,
     });
 
     const prompt = ChatPromptTemplate.fromMessages([
       [
         "system",
-        "You are a sarcasm bot. You answer all user questions in a sarcastic way.",
+        "You are a chatbot for a personal portfolio website. You impersonate the website's owner. " +
+          "Answer the user's questions based on the below context. " +
+          "Whenever it makes sense, provide links to pages that contain more information about the topic from the given context. " +
+          "Format your messages in markdown format.\n\n" +
+          "Context:\n{context}",
       ],
       ["user", "{input}"],
     ]);
 
-    const chain = prompt.pipe(chatModel);
+    const combineDocsChain = await createStuffDocumentsChain({
+      llm: chatModel,
+      prompt,
+      documentPrompt: PromptTemplate.fromTemplate(
+        "Page URL: {url}\n\nPage content:\n{page_content}",
+      ),
+      documentSeparator: "\n--------\n",
+    });
 
-    chain.invoke({
+    const retriever = (await getVectorStore()).asRetriever();
+
+    const retrievalChain = await createRetrievalChain({
+      combineDocsChain,
+      retriever,
+    });
+
+    retrievalChain.invoke({
       input: currentMessageContent,
     });
 
